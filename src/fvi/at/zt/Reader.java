6c169a50c67b27a69ba666bc403816aa5e77bf38
Victoria Frantsuh <eboko1@gmail.com>
2016-10-28 10:44:16 +0300

    add swing JOptionPane

--- src/fvi/at/zt/Reader.java
+++ src/fvi/at/zt/Reader.java
@@ -1,19 +1,25 @@
 package fvi.at.zt;
 
-import java.util.Scanner;
-
-/**
- * Created by Vika on 28.10.2016.
- */
+import javax.swing.JOptionPane;
 public class Reader {
-    int i;
-    int k;
+    int i,k;
+    String line1,line2;
+
     public void Scan(){
-        System.out.println("Введіть перше число ");
-       Scanner scn= new Scanner(System.in);
-         i=scn.nextInt();
-        System.out.println("Введіть наступне число ");
-        k=scn.nextInt();
+       // System.out.println("Введіть перше число ");
+       //Scanner scn= new Scanner(System.in);
+        // i=scn.nextInt();
+        //System.out.println("Введіть наступне число ");
+        //k=scn.nextInt();
+        line1= JOptionPane.showInputDialog("Введіть перше число ");
+        line2= JOptionPane.showInputDialog("Введіть наступне число ");
+        i=Integer.parseInt(line1);
+        k=Integer.parseInt(line2);
+    }
+    public void Scan(int a, int b){
+        JOptionPane.showInputDialog("Тепер ваше перше число = "+a);
+        JOptionPane.showInputDialog(" ваше наступне число = "+b);
+
     }
 
 }
