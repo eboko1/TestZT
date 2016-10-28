00917bdab4f36aaf3deea0c1268237aa8bed9ee5
Victoria Frantsuh <eboko1@gmail.com>
2016-10-28 13:29:39 +0300

    add swing JOptionPane

--- src/fvi/at/zt/Dick.java
+++ src/fvi/at/zt/Dick.java
@@ -1,13 +1,20 @@
 package fvi.at.zt;
 
+import javax.swing.*;
+
 public class Dick {
-    public static  void main (String [] args){
-        Reader myreader=new Reader();
-        myreader.Scan();
+    public static void main(String[] args) {
+        Reader myreader = new Reader("GUI");
+        myreader.setVisible(true);// зробити вікно видиме
+        myreader.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// то як закривається вікно
+        myreader.setSize(300,200);// задає розмір вікна
+        myreader.setResizable(false); // неможемо змiнювати розміри вікна
+        myreader.setLocationRelativeTo(null); // розміщення по ценру
+        /*myreader.Scan();
         myreader.i=count(myreader.i);
         myreader.k=count(myreader.k);
-        //System.out.println("Тепер ваше перше число = " +myreader.i);
-        //System.out.println("Тепер ваше насупне число = " +myreader.k);
+        //1//System.out.println("Тепер ваше перше число = " +myreader.i);
+        //1//System.out.println("Тепер ваше насупне число = " +myreader.k);
         myreader.Scan(myreader.i, myreader.k);
     }
 
@@ -15,5 +22,6 @@ public class Dick {
          x=x+1;
         return x;
 
+    }*/
     }
 }
--- src/fvi/at/zt/Reader.java
+++ src/fvi/at/zt/Reader.java
@@ -1,25 +1,83 @@
 package fvi.at.zt;
 
-import javax.swing.JOptionPane;
-public class Reader {
-    int i,k;
-    String line1,line2;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-    public void Scan(){
-       // System.out.println("Введіть перше число ");
+public class Reader extends JFrame{
+    JButton b1,b2;
+    JLabel l1,l2,l3,l4;
+    JTextField t1,t2;
+    int i,k;
+    String a,b;
+    MyListener myLis= new MyListener();
+    //String line1,line2;
+    public Reader(String s){
+        super(s);
+        setLayout(new FlowLayout());//Передаємо значення положення елементів поруч
+        b1=new JButton("Очистити ");// кнопка чистить поля ввода для користувача пояснення
+        b2 =new JButton("Підрахувати ");
+        l1=new JLabel("Введіть перше число ");
+        l2=new JLabel("Введіть наступне число ");
+        l3=new JLabel("");
+        l4=new JLabel("");
+        t1=new JTextField(10);// просранство 10 символів
+        t2=new JTextField(10);
+        // + на екран в такому порядку програма + построчно
+
+
+        add(t1);
+        add(l1);
+
+        add(t2);
+        add(l2);
+        add(l3);
+        add(l4);
+        add(b2);
+        add(b1);
+        b2.addActionListener(myLis);
+        b1.addActionListener(myLis);
+    }
+public class MyListener implements ActionListener{
+
+    @Override
+    public void actionPerformed(ActionEvent e) {//метод визивається коли щось виконується на ПК
+        if (e.getSource()==b2){ //повертає значення і == кнопці підрахувати
+            i=Integer.parseInt(t1.getText());
+            k=Integer.parseInt(t2.getText());
+            i++;
+            k++;
+            l3.setText(a);
+            l4.setText(b);
+            a="Ваше перше число = "+i;
+            b="Наступне число = "+k;
+             }
+             if (e.getSource()==b1){
+                 t1.setText(null);
+                 t2.setText(null);
+                 l3.setText(null);
+                 l4.setText(null);
+
+             }
+    }
+}
+
+
+    //public void Scan(){
+       //1// System.out.println("Введіть перше число ");
        //Scanner scn= new Scanner(System.in);
         // i=scn.nextInt();
         //System.out.println("Введіть наступне число ");
-        //k=scn.nextInt();
-        line1= JOptionPane.showInputDialog("Введіть перше число ");
-        line2= JOptionPane.showInputDialog("Введіть наступне число ");
-        i=Integer.parseInt(line1);
-        k=Integer.parseInt(line2);
-    }
-    public void Scan(int a, int b){
-        JOptionPane.showInputDialog("Тепер ваше перше число = "+a);
-        JOptionPane.showInputDialog(" ваше наступне число = "+b);
+        //1//k=scn.nextInt();
 
-    }
+       //2// line1= JOptionPane.showInputDialog("Введіть перше число ");
+        //line2= JOptionPane.showInputDialog("Введіть наступне число ");
+        //i=Integer.parseInt(line1);
+        //k=Integer.parseInt(line2);}
+
+       //2//  public void Scan(int a, int b){
+       //      JOptionPane.showInputDialog("Тепер ваше перше число = "+a);
+       //      JOptionPane.showInputDialog(" ваше наступне число = "+b);}
 
 }
